4450a3d98f504efec190cdc87014b555e31443ef
RVESH <rishabhverma.rv2006@gmail.com>
2026-05-18 01:09:32 +0530

    false

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,7 +6,6 @@ import './style.scss';
 import { useAuth } from "../AuthContext";
 
 
-
 const CATEGORIES = [
   { emoji: '💄', label: 'Women Essentials',          sub: 'Cosmetics, hair care & jewellery',       slug: 'women-essentials'    },
   { emoji: '🛒', label: 'Grocery & FMCG',            sub: 'Snacks, staples & daily items',           slug: 'grocery-fmcg'        },
