340a1468b0478199e860d69b9d6306341984a7d2
RVESH <rishabhverma.rv2006@gmail.com>
2026-05-24 22:18:32 +0530

    false

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -33,6 +33,7 @@ function App() {
                   <Route path="/contact"     element={<pages.Contact />} />
                   <Route path="/admin"       element={<pages.Admin />} />
 
+
                   <Route path="/AdminOrders"element={<pages.AdminOrders />} />
                   <Route path="/signup"          element={<Signup />} />
                   <Route path="/login"           element={<Login />} />
